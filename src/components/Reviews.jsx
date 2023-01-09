64571fa55bc103bf035fc33de29d8161f1895305
johnnyfwk <johnnyfong@live.com>
2023-01-09 16:39:44 +0000

    Updated plan, included ReviewsByCategory, UserProfile, CreateAnAccount, and Login.

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,4 +1,14 @@
+import { useState, useEffect } from 'react';
+
 function Reviews() {
+    const [reviews, updateReviews] = useState( {} );
+    const [categories, updateCategories] = useState( [] );
+    const [selectedCategory, updateSelectedCategory] = useState( "" );
+
+    useEffect(() => {
+        
+    }, [])
+
     return (
         <section>
             <h2>Reviews</h2>
---
+++ src/utils/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const gamesBaseUrl = axios.create({
+    baseURL: ''
+})
+
+export function getCategories() {
+
+}
