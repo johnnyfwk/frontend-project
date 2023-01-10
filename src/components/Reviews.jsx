7bbfa31513e4800feef7ac84b0f1929033a5a20c
johnnyfwk <johnnyfong@live.com>
2023-01-10 11:21:31 +0000

    Committing changes for Task 4 'View all review' for review.

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,60 +1,23 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import * as api from '../utils/api';
 import ReviewCard from './ReviewCard';
 
-function Reviews( {reviews, updateReviews, categories, updateCategories} ) {
-    const [selectedCategory, updateSelectedCategory] = useState( "all" );
-
+function Reviews( {reviews, updateReviews} ) {
     useEffect(() => {
-        api.getReviews(selectedCategory)
+        api.getReviews()
             .then((response) => {
-                if (selectedCategory === "all") {
-                    updateReviews(response);
-                }
-                else {
-                    const reviewsByCategory = response.filter((review) => {
-                        return review.category === selectedCategory;
-                    })
-                    updateReviews(reviewsByCategory);
-                }
-            })
-    }, [selectedCategory]);
-
-    useEffect(() => {
-        api.getCategories()
-            .then((response) => {
-                updateCategories(response.map((category) => category.slug));
+                updateReviews(response);
             })
     }, []);
-
-    const categoriesToRender = [ 'all', ...categories ];
-
-    const categoriesToRenderInDropdown = categoriesToRender.map((category) => {
-        return <option key={category} value={category}>{category}</option>
-    })
-
-    function changeCategory(event) {
-        updateSelectedCategory(event.target.value);
-    }
 
     return (
         <main>
             <h1>Reviews</h1>
-            <form id="form-dropdown-categories">
-                <select name="dropdown-categories" id="dropdown-categories" form="form-dropdown-categories" onChange={changeCategory}>
-                {categoriesToRenderInDropdown}
-                </select>
-            </form>
-
-            <br />
-
             <section id='review-cards'>
                 {reviews.map((review) => {
                     return <ReviewCard key={review.review_id} review={review}/>
                 })}
             </section>
-            
-            
         </main>
     )
 }
