0a59136f782aaf4196588b2ee65008a457dee8a3
johnnyfwk <johnnyfong@live.com>
2023-01-12 17:13:08 +0000

    Committing task 9 for review.

--- src/App.js
+++ src/App.js
@@ -9,13 +9,11 @@ import Reviews from './components/Reviews';
 import Footer from './components/Footer';
 import Error404 from './components/Error404';
 import SingleReview from './components/SingleReview';
-import Categories from './components/Categories';
-import SingleCategory from './components/SingleCategory';
 
 function App() {
   const [usernameLoggedIn, setUsernameLoggedIn] = useState( "tickle122" );
   const [reviews, setReviews] = useState( [] );
-  const [ categories, setCategories ] = useState( [] );
+  const [categories, setCategories] = useState( [] );
 
   return (
     <div className="App">
@@ -25,11 +23,9 @@ function App() {
           <Route path="/" element={<Home />}></Route>
           <Route
             path="/reviews"
-            element={<Reviews reviews={reviews} setReviews={setReviews} />}>
+            element={<Reviews reviews={reviews} setReviews={setReviews} categories={categories} setCategories={setCategories}/>}>
           </Route>
           <Route path="/reviews/:review_id" element={<SingleReview usernameLoggedIn={usernameLoggedIn}/>}></Route>
-          <Route path="/categories" element={<Categories />}/>
-          <Route path="/categories/:category" element={<SingleCategory setCategories={setCategories}/>} />
           <Route path="/*" element={<Error404 />}></Route>
         </Routes>
         <Footer />
--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,18 +1,37 @@
 import { useState, useEffect } from "react";
+import { useSearchParams, Link } from "react-router-dom";
 import * as api from '../utils/api';
+import * as utils from '../utils/utils';
 import ReviewCard from './ReviewCard';
 
-function Reviews( {reviews, setReviews} ) {
-    const [areReviewsLoading, setAreReviewsLoading] = useState(true);
+function Reviews( {reviews, setReviews, categories, setCategories} ) {
+    const [areReviewsLoading, setAreReviewsLoading] = useState( true );
+    const [searchParams, setSearchParams] = useSearchParams();
+    const categorySearchParam = searchParams.get( 'category' );
 
     useEffect(() => {
         setAreReviewsLoading(true);
         api.getReviews()
             .then((response) => {
-                setReviews(response);
+                if (categorySearchParam === null) {
+                    setReviews(response);
+                }
+                else if (categorySearchParam) {
+                    setReviews(response.filter((element) => element.category === categorySearchParam));
+                }                
                 setAreReviewsLoading(false);
             })
-    }, []);
+            .catch((error) => {
+                console.log(error);
+            })
+    }, [categorySearchParam]);
+
+    useEffect(() => {
+        api.getCategories()
+            .then((response) => {            
+                setCategories(response.map((element) => element.slug))
+            })
+    }, [])
 
     if (areReviewsLoading) {
         return <p>Loading...</p>
@@ -21,6 +40,13 @@ function Reviews( {reviews, setReviews} ) {
     return (
         <main>
             <h1>Reviews</h1>
+
+            <div id="category-links">
+                {categories.map((category) => {
+                    return <Link key={category} to={`/reviews?category=${category}`}>{utils.createUserFriendlyCategoryName(category)}</Link>
+                })}
+            </div>            
+
             <section id='review-cards'>
                 {reviews.map((review) => {
                     return <ReviewCard key={review.review_id} review={review}/>
